d66561c589a784331aced24f9c2e7ce72b83a150
Robert Whitehead <rwhitehead130@gmail.com>
2014-12-08 12:56:43 +1300

    2014-12-08 : 17

--- www/js/index.js
+++ www/js/index.js
@@ -38,6 +38,24 @@ var app = {
     }
     
     foundLocation: function() {
+		var map;
+		function initialize() {
+		  var mapOptions = {
+			  mapTypeControlOptions: {
+		        mapTypeIds: ['UC']
+		    },
+		    center: new google.maps.LatLng(-43.523893, 172.583649),
+		    zoom: 15,
+		    scrollwheel: false,
+		    mapTypeId: 'UC'
+		  };
+		  map = new google.maps.Map(document.getElementById('map-canvas'),
+		      mapOptions);
+		    var styledMapType = new google.maps.StyledMapType(styles, {name: 'UC'});
+		    map.mapTypes.set('UC', styledMapType);
+		}
+		
+		google.maps.event.addDomListener(window, 'load', initialize);
 	}
 	
 	noLocation: function() {
