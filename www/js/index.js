c10bf3ec35374e55e62415fe62e00340721779de
Robert Whitehead <rwhitehead130@gmail.com>
2014-12-08 16:08:48 +1300

    2014-12-08 : 25

--- www/js/index.js
+++ www/js/index.js
@@ -47,70 +47,17 @@ var app = {
 	      mapOptions);
 	    var styledMapType = new google.maps.StyledMapType(styles, {name: 'UC'});
 	    map.mapTypes.set('UC', styledMapType);
-	    
-		var overlay;
-		UCOverlay.prototype = new google.maps.OverlayView();
-		
-		var swBound = new google.maps.LatLng(-43.524570, 172.581371);
-		var neBound = new google.maps.LatLng(-43.523146, 172.583988);
-		var bounds = new google.maps.LatLngBounds(swBound, neBound);
-		  
-		var srcImage = "img/transparency";
 	
-		overlay = new UCOverlay(bounds, srcImage, map);
-
-		UCOverlay.prototype.onAdd = function() {
-		
-		  var div = document.createElement('div');
-		  div.style.borderStyle = 'none';
-		  div.style.borderWidth = '0px';
-		  div.style.position = 'absolute';
-		  
-		  var img = document.createElement('img');
-		  img.src = this.image_;
-		  img.style.width = '100%';
-		  img.style.height = '100%';
-		  img.style.position = 'absolute';
-		  div.appendChild(img);
-		
-		  this.div_ = div;
-		
-		  var panes = this.getPanes();
-		  panes.overlayLayer.appendChild(div);
-		};
-		
-		UCOverlay.prototype.draw = function() {
-		
-		  var overlayProjection = this.getProjection();
-		
-		  var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
-		  var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
-
-		  var div = this.div_;
-		  div.style.left = sw.x + 'px';
-		  div.style.top = ne.y + 'px';
-		  div.style.width = (ne.x - sw.x) + 'px';
-		  div.style.height = (sw.y - ne.y) + 'px';
-		};
-		
-		USGSOverlay.prototype.onRemove = function() {
-		  this.div_.parentNode.removeChild(this.div_);
-		  this.div_ = null;
-		};
 	},
 	
 	onError: function(error) {
 		alert(error.message);
-	},
-	
-	UCOverlay: function(bounds, image, map) {
-
-		  this.bounds_ = bounds;
-		  this.image_ = image;
-		  this.map_ = map;
-
-		  this.div_ = null;
-
-		  this.setMap(map);
 	}
+
 };
+
+//var swBound = new google.maps.LatLng(-43.524570, 172.581371);
+//var neBound = new google.maps.LatLng(-43.523146, 172.583988);
+//var bounds = new google.maps.LatLngBounds(swBound, neBound);
+
+//var srcImage = "img/transparency";
